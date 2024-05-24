586fd66d3f2be1d73486208805160c4f4ec81d7a
Rodrigo <rodrigo1cabrera2@gmail.com>
2024-05-25 02:49:07 +0800

    Add helmet

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,33 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // VALIDATION
   app.useGlobalPipes(
     new ValidationPipe({
       disableErrorMessages: process.env.NODE_ENV === 'production',
     }),
   );
+
+  // SWAGGER
   const config = new DocumentBuilder()
     .setTitle('Mateada API')
     .setDescription('The Mateada API description')
     .setVersion('1.0')
     .addTag('mateada')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
+
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(8080);
+  // HELMET
+  app.use(helmet());
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
